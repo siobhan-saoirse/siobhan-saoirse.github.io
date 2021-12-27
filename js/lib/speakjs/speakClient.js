fb4f4e2b7703bb297190f444dd9c80cdc01ad1fb
CrazyMediumScout <33694155+Seamusmario@users.noreply.github.com>
2021-12-27 18:14:57 -0500

    Update speakClient.js

--- js/lib/speakjs/speakClient.js
+++ js/lib/speakjs/speakClient.js
@@ -47,7 +47,7 @@ function speak(text, args) {
             }
             return ret;
         }
-        document.getElementById("audio").innerHTML = '<audio id="player" src="data:audio/x-wav;base64,' + encode64(wav) + '" autoplay>';
+        document.getElementById("audio").innerHTML = '<video id="player" src="data:audio/x-wav;base64,' + encode64(wav) + '" autoplay>';
         document.getElementById("player").play();
     }
     function playAudioDataAPI(data) {
